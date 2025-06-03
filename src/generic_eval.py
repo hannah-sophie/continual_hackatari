75a204a759fda6c91d319bc35bc7aab925cbce35
Lars Reining <lars.reining@stud.tu-darmstadt.de>
2025-06-03 11:23:15 +0200

    Fix and move generic eval

--- src/generic_eval.py
+++ src/generic_eval.py
@@ -1,27 +1,24 @@
 from typing import Callable
 
-import gymnasium as gym
 import torch
+from stable_baselines3.common.vec_env import SubprocVecEnv
 
 
 def evaluate(agent, make_env: Callable, eval_episodes: int, device, **env_kwargs):
-    envs = gym.vector.SyncVectorEnv([make_env(idx=0, **env_kwargs)])
+    env = SubprocVecEnv([make_env(idx=0, **env_kwargs)])
     agent.eval()
-
-    obs, _ = envs.reset()
+    obs = env.reset()
     episodic_returns = []
-    counter = 0
     while len(episodic_returns) < eval_episodes:
-        if counter > 100:
-            break
-        counter += 1
         actions, _, _, _ = agent.get_action_and_value(torch.Tensor(obs).to(device))
-        next_obs, _, _, _, infos = envs.step(actions.cpu().numpy())
-        if "final_info" in infos:
-            for info in infos["final_info"]:
+        next_obs, _, next_done, infos = env.step(actions.cpu().numpy())
+        if 1 in next_done:
+            for info in infos:
                 if "episode" not in info:
                     continue
+                print("here")
                 episodic_returns += [info["episode"]["r"]]
+        print(episodic_returns)
         obs = next_obs
 
     return episodic_returns
